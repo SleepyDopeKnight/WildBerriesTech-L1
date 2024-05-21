662d7eaf6785aa7a8de139680f3d448fc6212429
Chamomile Valentin <chamomiv@pr-h5.kzn.21-school.ru>
2024-05-21 23:13:37 +0300

    ended 4

--- tasks/04_channel_workers/main.go
+++ tasks/04_channel_workers/main.go
@@ -3,64 +3,71 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"os/signal"
 	"sync"
+	"time"
 )
 
 func main() {
-	stopSignal, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	gracefulShutdown(cancel)
+	stopSignal, cancel := context.WithCancel(context.Background()) // создает контекст, с помощью которого можно выйти из программы
+	defer cancel()                                                 // для отмены созданого контекста
+	gracefulShutdown(cancel)                                       // выход из программы на ctlc+c
 	fmt.Println("Count of workers:")
 	var workers int
 	var wg sync.WaitGroup
-	fmt.Fscan(os.Stdin, &workers)
-	data := make(chan int)
-	go generateData(stopSignal, data)
-	for i := 1; i < workers; i++ {
-		wg.Add(1)
-		go worker(stopSignal, data, &wg)
+	if _, err := fmt.Fscan(os.Stdin, &workers); err != nil || workers <= 0 {
+		log.Fatal("Invalid number of workers") // считываем из stdin количество работников
 	}
+	data := make(chan int)            // создаем канал, в который будем записывать данные
+	go generateData(stopSignal, data) // запускаем горутину, в которую передаем канал для записи данных и контекст, для завершения работы рутины
+	for i := 0; i < workers; i++ {
+		wg.Add(1)                        // добавление счетчика для каждой горутины
+		go worker(stopSignal, data, &wg) // запуск работников
+	}
+	defer close(data) // закрываем канал
+	wg.Wait()         // ожидание завершения работы всех горутин
 }
 
 func worker(ctx context.Context, data <-chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
+	defer wg.Done() // уменьшение счетчика для каждой горутины
 
 	for {
 		select {
-		case <-ctx.Done():
-			fmt.Println("Exit from program")
+		case <-ctx.Done(): // если пришел контекст выхода из программы, то завершаем программу
+			fmt.Println("Worker exiting")
 			return
-		default:
-			fmt.Println(<-data)
+		case number, ok := <-data: // записываем сгенерированные данные в канал
+			if !ok { // проверяем, не закрылся ли канал
+				fmt.Println("Channel is closed")
+				return
+			}
+			fmt.Println(number)
 		}
 	}
 }
 
 func generateData(ctx context.Context, data chan<- int) {
-	defer close(data)
 	for {
 		select {
-		case <-ctx.Done():
-			fmt.Println("Exit from program")
+		case <-ctx.Done(): // если пришел контекст выхода из программы, то завершаем программу
+			fmt.Println("Stop generating")
 			return
 		default:
-			data <- rand.Int()
+			time.Sleep(time.Second)
+			data <- rand.Int() // записываем сгенерированные данные в канал для чтения работниками
 		}
 	}
 }
 
-func start(ctx context.Context) {
-}
-
 func gracefulShutdown(cancel context.CancelFunc) {
 	go func() {
-		signalChan := make(chan os.Signal, 1)
-		signal.Notify(signalChan, os.Interrupt)
+		signalChan := make(chan os.Signal)      // канал, принмающий сигнала системы
+		signal.Notify(signalChan, os.Interrupt) // оповещает канал о сигнале, полученный от os.Interrupt(подписывает канал на получения сигнала)
 		defer signal.Stop(signalChan)
-		<-signalChan
-		cancel()
+		<-signalChan // ожидаю сигнала стоп от канала
+		cancel()     // отменяем созданный контекстм
 	}()
 }
